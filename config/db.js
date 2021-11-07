6506d21aa750874b0d95b106286b3956b8b47dc9
Rujaxx <shaikhrujan@gmail.com>
2021-11-07 14:47:14 +0530

    database connected

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const { Sequelize } = require('sequelize');
+
+
+const dotenv = require('dotenv')
+
+//load env vars
+dotenv.config({ path : './config/config.env'})
+
+module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: 'localhost',
+    dialect:  'postgres'
+});
--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const db = require('./config/db')
 
 //Env Vars
 dotenv.config({ path : './config/config.env'})
+
+
+//
+// db.sync({ force: true }).then(() => {
+//     console.log("Drop and re-sync db.");
+//   });    
+
+//Check db
+db.authenticate().
+then(console.log('Connection has been established successfully.'))
+.catch(error => console.error('Unable to connect to the database:', error))
 
 const app = express()
 
